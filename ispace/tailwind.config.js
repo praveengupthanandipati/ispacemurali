45ca9cf10553c9d0bddf13cdd5a259c191aa4bd5
Murali Nagabrahmam Thatholu <murali.thatholu@ispace.com>
2023-01-10 13:58:31 +0530

    Header changes implemented

--- ispace/tailwind.config.js
+++ ispace/tailwind.config.js
@@ -2,44 +2,9 @@
 module.exports = {
   content: ['./build/*.html'],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    container: {
+      padding: '2rem',
     },
-    colors: {
-      'blue': '#1fb6ff',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-    },
-    padding: {
-      '9px': '9px',
-      '10px': '10px',
-      '17px': '17px',
-      '2px': '2px'
-    },
-    minWidth: {
-       '0': '0',
-       '1/4': '25%',
-       '1/2': '50%',
-       '3/4': '75%',
-       'full': '100%',
-       '280' : '280%'
-      },
-      borderWidth: {
-        DEFAULT: '1px',
-        '0': '0',
-        '2': '2px',
-        '3': '3px',
-        '4': '4px',
-        '6': '6px',
-        '8': '8px',
-      },
     extend: {
       backgroundImage: {
    
